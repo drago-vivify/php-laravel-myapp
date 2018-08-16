dc4cc4a71658ec73cc73ce46a5f60be65dba3acc
Drago <drago.s@vivifyideas.com>
2018-08-16 12:16:21 +0200

    DSWC-8 Registration

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -62,6 +62,13 @@
                                 </a>
                             </div>
                         </div>
+
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <a class="btn btn-link" href="{{ route('register') }}">Don't have an account? Register here</a>
+                            </div>
+                        </div>
                     </form>
                 </div>
             </div>
